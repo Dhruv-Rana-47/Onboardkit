ddfb8be2adccfb4e78fed9c5987693b83fa5b517
Dhruv-Rana-47 <ranadhruv842@gmail.com>
2025-06-16 21:45:28 +0530

    Add code for creating superuser for  deployment

--- onboardkit/config/wsgi.py
+++ onboardkit/config/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
+
+# config/wsgi.py
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+from django.db import connections
+from django.db.utils import OperationalError
+
+User = get_user_model()
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')
+except OperationalError:
+    pass  # DB not ready yet
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-
 application = get_wsgi_application()
